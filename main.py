7cfef864d483ddc2bf1605907546b0ecabbcfc24
AhlamHasan <69400247+AhlamHasan@users.noreply.github.com>
2022-06-03 00:30:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ with st.form(key='nlpForm'):
 			Requirements = st.text_area("Enter Requirement")
 			submit_button = st.form_submit_button(label='Detect defects')
 
-df = pd.read_excel(r"C:\Users\ASUS\Downloads\Book2.xlsx")
+df = pd.read_excel('Book2.xlsx')
 
 
 Unbounded_Lists = df['Unbounded Lists'].values.tolist()
